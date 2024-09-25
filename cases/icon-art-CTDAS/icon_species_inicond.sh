8cab10bb2609ebf73735889ae71aa8ce5d28f2b5
efkmoene <erikkoene@hotmail.com>
2024-09-25 15:06:40 +0200

    Trying a few variations of vertically interpolating CAMS onto ERA5

--- cases/icon-art-CTDAS/icon_species_inicond.sh
+++ cases/icon-art-CTDAS/icon_species_inicond.sh
@@ -2,74 +2,16 @@
 
 cd {cfg.icon_input_icbc}
 
+# Compute Pressure levels from hybrid model levels (CAMS: 80 levels)
+ncap2 -s 'P_level[hlevel,latitude,longitude]=ap+bp*Psurf' {cams_file} -O cams_pressure.nc
+ncap2 -s 'P_level_avg[level,latitude,longitude]=(P_level(1:$hlevel.size-1,:,:)+P_level(0:$hlevel.size-2,:,:))/2' cams_pressure.nc -O cams_pressure_avg.nc
 
-species2restart=($(echo {cfg.species2restart} | tr -d '[],'))
+# Compute Pressure levels from hybrid model levels (IFS: 137 levels). The added complexity lies in the need to rename dimension 'nhym' to 'lev'
+ncap2 -s 'P_level[time,nhym,ncells]=hyam+hybm*PS' era_2018010100_ini.nc -O era5_pressure.nc
+ncks -v P_level era5_pressure.nc -O tmp1.nc
+ncks -O -x -v P_level era5_pressure.nc tmp2.nc
+ncrename -d nhym,lev tmp1.nc -O tmp3.nc
+ncks -h -A tmp3.nc tmp2.nc
+mv tmp2.nc era5_pressure.nc
 
-
-if [[ {cfg.lrestart} == '.FALSE.' ]]; then
-
-    # ----------------------------------------------------------
-    # -- Replicate Q and GEOSP variables for ICON-ART
-    # ----------------------------------------------------------
-
-    cdo expr,'Q=QV;' {filename} tmp.nc
-    ncks -A -v Q tmp.nc {filename}
-    rm tmp.nc
-
-    cdo expr,'GEOP_SFC=GEOSP;' {filename} tmp.nc
-    ncks -A -v GEOP_SFC tmp.nc {filename}
-    rm tmp.nc
-
-fi
-
-# ----------------------------------------------------------
-# -- Create CH4 and CO variables (if CAMS not available)
-# ----------------------------------------------------------
-if [[ "${{species2restart[*]}}" =~ "TRCH4" || "${{species2restart[*]}}" =~ "TRCO" ]]; then
-
-    # ----------------------------------------------------------
-    # -- Remap CAMS data (if CAMS available...)
-    # ----------------------------------------------------------
-
-    # # -- Convert the GRIB files to NetCDF
-    # cdo -t ecmwf -f nc copy cams.grib cams.nc
-
-    # # -- Retrieve the dynamic horizontal grid
-    # cdo -s selgrid,2 {cfg.input_files_scratch_dynamics_grid_filename} triangular-grid.nc
-
-    # # -- Remap
-    # cdo -s remapdis,triangular-grid.nc cams.nc cams_final.nc
-    # rm cams.nc 
-
-    # # -- Merge CAMS and ERA5 data
-    # ncks -h -A cams.nc tmp.nc
-    # rm cams.nc cams.grib
-
-    # # -- Rename variables
-    # ncrename -h CH4,TRCH4 tmp.nc
-    # ncrename -h CO,TRCO tmp.nc
-    # ncks tmp.nc {filename} 
-    # rm tmp.nc
-
-    # ----------------------------------------------------------
-    # -- Or just create basic variables
-    # ----------------------------------------------------------
-    var_tracer="TRCH4{ext_restart}"
-    cdo expr,"TRCH4=QV / QV * 0.000002;" {filename} tmp.nc
-    if [ ! -z "{ext_restart}" ] ; then
-        ncrename -h -v TRCH4,${{var_tracer}} tmp.nc 
-    fi 
-    ncks -A -v ${{var_tracer}} tmp.nc {filename}
-    rm tmp.nc
-
-    var_tracer="TRCO{ext_restart}"
-    cdo expr,"TRCO=QV / QV * 0.000002;" {filename} tmp.nc
-    if [ ! -z "{ext_restart}" ] ; then
-        ncrename -h -v TRCO,${{var_tracer}} tmp.nc 
-    fi 
-    ncks -A -v ${{var_tracer}} tmp.nc {filename}
-    rm tmp.nc
-
-
-fi
-
+rm tmp*.nc
